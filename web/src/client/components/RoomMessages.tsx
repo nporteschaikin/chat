551cb8d7cf3273554c444c178fbbbe7c2eb9689a
Noah Portes Chaikin <noah@porteschaikin.com>
2020-03-27 17:20:07 -0400

    Apply better scroll behavior on room messages

--- web/src/client/components/RoomMessages.tsx
+++ web/src/client/components/RoomMessages.tsx
@@ -12,16 +12,15 @@ const RoomMessages: React.FC<{ room: Room }> = ({ room }) => {
   const { state } = React.useContext(store)
 
   const ref = React.useRef<HTMLDivElement>(null)
-  const { current } = ref
 
   const messages = state.roomMessages[room.id]
   const onMessagesLengthChange = () => {
-    if (current === null) return
+    if (ref.current !== null) {
+      const { scrollTop, scrollHeight, offsetHeight, clientHeight } = ref.current!
 
-    const { scrollTop, scrollHeight, offsetHeight, clientHeight } = current!
-
-    if (scrollTop > scrollHeight - offsetHeight - clientHeight * 0.25) {
-      current.scrollTop = scrollHeight
+      if (scrollTop === 0 || scrollTop > scrollHeight - offsetHeight - clientHeight * 0.25) {
+        ref.current!.scrollTop = ref.current!.scrollHeight
+      }
     }
   }
 
