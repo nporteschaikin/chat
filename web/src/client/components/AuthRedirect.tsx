feadfad187a22e618f8e5bbbe5933b4a721f9b55
Noah Portes Chaikin <noah@porteschaikin.com>
2020-03-27 17:53:21 -0400

    Fix `AuthRedirect`
    use `buildRoomLocationPathFromRoom`

--- web/src/client/components/AuthRedirect.tsx
+++ web/src/client/components/AuthRedirect.tsx
@@ -2,25 +2,14 @@ import * as React from "react"
 import { Redirect } from "react-router-dom"
 
 import { store } from "./../store"
-
-const DEFAULT_ROOM_HANDLE = "general"
+import { buildRoomLocationPathFromRoom } from "./../helpers/rooms"
 
 const AuthRedirect: React.FC = () => {
   const { state } = React.useContext(store)
   const { lastOpenRoom } = state
   const firstStarredRoom = state.rooms.find((room) => room.starred) || null
 
-  return (
-    <Redirect
-      to={`/r/${
-        lastOpenRoom
-          ? lastOpenRoom.handle
-          : firstStarredRoom
-          ? firstStarredRoom.handle
-          : DEFAULT_ROOM_HANDLE
-      }`}
-    />
-  )
+  return <Redirect to={buildRoomLocationPathFromRoom(lastOpenRoom || firstStarredRoom)} />
 }
 
 export default AuthRedirect
